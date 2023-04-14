9e83d56a89627ecd9aba1002cf64e15521351716
Nikita <Bnikita72@yandex.ru>
2023-04-14 11:57:04 +0300

    jobbb

--- index.js
+++ index.js
@@ -1,38 +1,36 @@
-const editButton = document.querySelector('.profile__editButton') // Клавиша редактирования
+const editButton = document.querySelector('.profile__edit-Button') // Клавиша редактирования
 
 // Находим форму в DOM
 let formElement = document.querySelector('.popup')
 
-let nameInput = document.querySelector('.popup__name') // Воспользуйтесь инструментом .querySelector()
-let jobInput = document.querySelector('.popup__job') // Воспользуйтесь инструментом .querySelector()
+let nameInput = document.querySelector('.popup__field_name') // Воспользуйтесь инструментом .querySelector()
+let jobInput = document.querySelector('.popup__field_job') // Воспользуйтесь инструментом .querySelector()
 
 let name = document.querySelector('.profile__name')
 let job = document.querySelector('.profile__job')
 
 // Находим поля формы в DOM
 function showPopup (evt) {
-    evt.preventDefault();
-    formElement.classList.toggle('popup__opened')
-    nameInput.value = (name.textContent)   // вставили имя
-    jobInput.value = (job.textContent) //вставили джоб
+    formElement.classList.add('popup__opened')
+    nameInput.value = name.textContent   // вставили имя
+    jobInput.value = job.textContent //вставили джоб
 } 
 editButton.addEventListener('click', showPopup);
 
 // закрытие
 const closeButton = document.querySelector('.popup__button-exit')
 function exitButton (evt) {
-    evt.preventDefault ();
-    formElement.classList.toggle('popup__opened') 
+    formElement.classList.remove('popup__opened') 
 }
 closeButton.addEventListener('click', exitButton);
 
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
+
 function handleFormSubmit (evt) {
     evt.preventDefault(); 
-    name.textContent = (nameInput.value)
-    job.textContent = (jobInput.value)
+    name.textContent = nameInput.value
+    job.textContent = jobInput.value
+    exitButton()
 }
 
 // Прикрепляем обработчик к форме:
